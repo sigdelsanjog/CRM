2676156b22f2bd8ec5d8d5936155e0e81d087ca3
sigdelsanjog <sigdelsanjog@gmail.com>
2020-08-11 08:57:54 +0545

    CRM App

---
+++ account/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Customer, Products, Order, Tag
+
+# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Products)
+admin.site.register(Tag)
+admin.site.register(Order)
